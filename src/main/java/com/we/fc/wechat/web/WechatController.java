3cae8b9a93143df201a364365272749a1e6e68f8
zdcsoul <451914442@qq.com>
2018-04-07 18:14:35 +0800

    微信后台页面路径修改

--- src/main/java/com/we/fc/wechat/web/WechatController.java
+++ src/main/java/com/we/fc/wechat/web/WechatController.java
@@ -2,10 +2,17 @@ package com.we.fc.wechat.web;
 
 import com.we.fc.base.BaseController;
 import com.we.fc.base.BaseService;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClients;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author zdc
@@ -23,6 +30,15 @@ public class WechatController extends BaseController {
     @GetMapping("index")
     public String index(Integer menuId, Model model){
         model.addAttribute("loopMenu", getMenuById(menuId));
-        return "wx/index";
+        return "wechat/index";
+    }
+
+    @PostMapping("createMenu")
+    @ResponseBody
+    public String createMenu(){
+        final String url = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=8_1oAGMGf_144ig1uOE5K_7JvdVmXT4kOFssXxDHgLT5ovwGHNWkD8BIivI2HGomQJoiswmaBMb60VMsMYxeAcC6WREJRO9py9Lia-xIVoPatYZE5t-dmaiqUkG-iSOSQAchAuubhvKobTDgygTZYjAGACIH";
+        HttpClient client = HttpClients.createDefault();
+        HttpPost post = new HttpPost();
+        return "ok";
     }
 }
