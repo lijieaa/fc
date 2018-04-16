0a7c205dcf3762c159ea48b91f4e0d9e1ebc871d
lijieaa <952054767@qq.com>
2018-04-16 23:14:06 +0800

    删除报错文件
    删除报错文件

--- src/main/java/com/we/fc/menu/dao/DingtalkMenuMapper.java
+++
@@ -1,9 +0,0 @@
-package com.we.fc.menu.dao;
-
-import com.we.fc.base.BaseDao;
-import com.we.fc.menu.entity.DingtalkMenu;
-import java.util.List;
-
-public interface DingtalkMenuMapper extends BaseDao<DingtalkMenu>{
-
-}
