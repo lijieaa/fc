dc27027d43db88280b45d29f87b545b1bad0e983
zhongke1214 <38214294+zhongke1214@users.noreply.github.com>
2018-04-30 15:06:34 +0800

    文件大小

--- src/main/java/com/we/fc/FcApplication.java
+++ src/main/java/com/we/fc/FcApplication.java
@@ -34,6 +34,10 @@ public class FcApplication extends SpringBootServletInitializer {
     MultipartConfigElement multipartConfigElement() {
         MultipartConfigFactory factory = new MultipartConfigFactory();
         factory.setLocation("/");
+        //文件最大
+        factory.setMaxFileSize("10240KB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("102400KB");
         return factory.createMultipartConfig();
     }
 
