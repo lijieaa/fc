2d70124a2a8c41c3524d08c67b525c5ed7615efc
zdcsoul <451914442@qq.com>
2018-04-30 16:57:48 +0800

    数据源添加到配置文件

--- src/main/java/com/we/fc/config/DataSourceConfig.java
+++ src/main/java/com/we/fc/config/DataSourceConfig.java
@@ -1,8 +1,11 @@
 package com.we.fc.config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import java.sql.SQLException;
 
@@ -11,26 +14,147 @@ import java.sql.SQLException;
  * @since 2018-04-06 15:14
  */
 @Configuration
+@PropertySource("classpath:datasource.properties")
+@ConfigurationProperties(prefix = "fc.datasource")
 public class DataSourceConfig {
+
+    private String username;
+
+    private String password;
+
+    private String url;
+
+    private String driverName;
+
+    private Integer initialSize;
+
+    private Integer maxActive;
+
+    private Integer minIdle;
+
+    private Long maxWait;
+
+    private Long minEvictableIdleTimeMillis;
+
+    private Integer queryTimeout;
+
+    private String validationQuery;
+
+    private Integer validationQueryTimeout;
 
     @Bean
     public DruidDataSource dataSource() throws SQLException {
         DruidDataSource druidDataSource = new DruidDataSource();
-        druidDataSource.setUsername("fc");
-        druidDataSource.setPassword("123456");
-        druidDataSource.setUrl("jdbc:mysql://47.104.144.238:6666/fc?characterEncoding=utf-8&useSSL=false&zeroDateTimeBehavior=convertToNull&&allowMultiQueries=true");
-        druidDataSource.setDriverClassName("com.mysql.jdbc.Driver");
-        druidDataSource.setInitialSize(5);
-        druidDataSource.setMaxActive(200);
-        druidDataSource.setMinEvictableIdleTimeMillis(300000);
-        druidDataSource.setMinIdle(5);
-        druidDataSource.setMaxWait(60000);
-        druidDataSource.setMinEvictableIdleTimeMillis(30 * 60 * 1000);
-        druidDataSource.setQueryTimeout(5);
-        druidDataSource.setValidationQuery("SELECT 1");
-        druidDataSource.setValidationQueryTimeout(3);
+        druidDataSource.setUsername(username);
+        druidDataSource.setPassword(password);
+        druidDataSource.setUrl(url);
+        druidDataSource.setDriverClassName(driverName);
+        druidDataSource.setInitialSize(initialSize);
+        druidDataSource.setMaxActive(maxActive);
+        druidDataSource.setMinIdle(minIdle);
+        druidDataSource.setMaxWait(maxWait);
+        druidDataSource.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        druidDataSource.setQueryTimeout(queryTimeout);
+        druidDataSource.setValidationQuery(validationQuery);
+        druidDataSource.setValidationQueryTimeout(validationQueryTimeout);
         System.out.println("正在初始化Druid...");
         druidDataSource.init();
         return druidDataSource;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    public Integer getInitialSize() {
+        return initialSize;
+    }
+
+    public void setInitialSize(Integer initialSize) {
+        this.initialSize = initialSize;
+    }
+
+    public Integer getMaxActive() {
+        return maxActive;
+    }
+
+    public void setMaxActive(Integer maxActive) {
+        this.maxActive = maxActive;
+    }
+
+    public Integer getMinIdle() {
+        return minIdle;
+    }
+
+    public void setMinIdle(Integer minIdle) {
+        this.minIdle = minIdle;
+    }
+
+    public Long getMaxWait() {
+        return maxWait;
+    }
+
+    public void setMaxWait(Long maxWait) {
+        this.maxWait = maxWait;
+    }
+
+    public Long getMinEvictableIdleTimeMillis() {
+        return minEvictableIdleTimeMillis;
+    }
+
+    public void setMinEvictableIdleTimeMillis(Long minEvictableIdleTimeMillis) {
+        this.minEvictableIdleTimeMillis = minEvictableIdleTimeMillis;
+    }
+
+    public Integer getQueryTimeout() {
+        return queryTimeout;
+    }
+
+    public void setQueryTimeout(Integer queryTimeout) {
+        this.queryTimeout = queryTimeout;
+    }
+
+    public String getValidationQuery() {
+        return validationQuery;
+    }
+
+    public void setValidationQuery(String validationQuery) {
+        this.validationQuery = validationQuery;
+    }
+
+    public Integer getValidationQueryTimeout() {
+        return validationQueryTimeout;
+    }
+
+    public void setValidationQueryTimeout(Integer validationQueryTimeout) {
+        this.validationQueryTimeout = validationQueryTimeout;
     }
 }
