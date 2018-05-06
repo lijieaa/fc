535774fa007476ac8d5442dd0c5d3ecf43e817ff
lijieaa <952054767@qq.com>
2018-05-06 17:25:16 +0800

    角色组添加更新
    角色组添加更新

--- src/main/java/com/we/fc/role/service/impl/RoleServiceImpl.java
+++ src/main/java/com/we/fc/role/service/impl/RoleServiceImpl.java
@@ -2,6 +2,8 @@ package com.we.fc.role.service.impl;
 
 import com.we.fc.base.BaseDao;
 import com.we.fc.base.BaseServiceImpl;
+import com.we.fc.menu.dao.MenuDao;
+import com.we.fc.menu.entity.Menu;
 import com.we.fc.role.dao.RoleDao;
 import com.we.fc.role.entity.Role;
 import com.we.fc.role.service.RoleService;
@@ -9,12 +11,18 @@ import com.we.fc.user.entity.DingtalkUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
+@Transactional
 public class RoleServiceImpl extends BaseServiceImpl<Role> implements RoleService {
 
     @Autowired
     RoleDao dao;
+
+
 
     @Override
     public BaseDao<Role> getDao() {
@@ -26,6 +34,28 @@ public class RoleServiceImpl extends BaseServiceImpl<Role> implements RoleServic
         DingtalkUser user = (DingtalkUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         Integer intermediaryId = user.getIntermediaryId();
         role.setIntermediaryId(intermediaryId);
-        return super.insert(role);
+        int insert = super.insert(role);
+        List<Menu> menus = role.getMenus();
+        for (Menu menu : menus) {
+            dao.insertRM(role.getId(),menu.getId());
+        }
+        return insert;
+    }
+
+    @Override
+    public void updateByPrimaryKey(Role role) throws Exception {
+        DingtalkUser user = (DingtalkUser) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Integer intermediaryId = user.getIntermediaryId();
+        role.setIntermediaryId(intermediaryId);
+
+        dao.deleteRM(role.getId());
+
+        List<Menu> menus = role.getMenus();
+        for (Menu menu : menus) {
+            dao.insertRM(role.getId(),menu.getId());
+        }
+
+
+        super.updateByPrimaryKey(role);
     }
 }
