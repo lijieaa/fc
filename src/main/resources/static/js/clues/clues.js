2a377b72b375c4986482a52b23bee4abef266f04
liuhong <1150487420@qq.com>
2018-05-17 21:33:53 +0800

    中间商

--- src/main/resources/static/js/clues/clues.js
+++ src/main/resources/static/js/clues/clues.js
@@ -1,3 +1,36 @@
+//中间商管理
+function showMenu() {
+    $(".tree-div-contain").show();
+    $("body").unbind("mousedown", onBodyDown);
+}
+function hideMenu() {
+    $(".tree-div-contain").hide();
+    $("body").unbind("mousedown", onBodyDown);
+}
+function onBodyDown(event) {
+    if (!(event.target.id == "address"  || event.target.className == "tree-div-contain" || $(event.target).parents(".tree-div-contain").length>0)) {
+        hideMenu();
+    }
+}
+$(document).bind("click",function(e){
+    onBodyDown(e) ;
+});
+$(document).on("click",".search-list >li",function(){
+    $(".tree-div-contain").hide();
+    $("#address").val($(this).text());
+    $("#treeDemo").show();
+    $(".search-list").hide();
+    var text = $(this).text();
+    var treeObj = $.fn.zTree.getZTreeObj("treeDemo");
+    var nodes = treeObj.transformToArray(treeObj.getNodes());
+    $(".limit-style").val("");
+    nodes.forEach(function(item,i){
+        if(nodes[i].name == text){
+            treeObj.selectNode(nodes[i]);
+        }
+    });
+
+});
 Vue.component('option-item', {
     props: ['todo'],
     template: '<option>{{todo.name}}</option>'
@@ -7,12 +40,34 @@ new Vue({
     data:{
         deviceName:"添加线索",
         inputLimit:"",
+        searchVal:"",
         selected:"3",
         projectList:[
             {val:1,name:"成都华西"},
             {val:2,name:"成都龙泉"},
             {val:3,name:"项目二"}
-        ]
+        ],
+        editList:{
+            area:{
+                name:"",
+                id:""
+            },
+            company: null,
+            createTime: "",
+            id: 1,
+            intermediaryContact: "",
+            intermediaryContactTel: "",
+            intermediaryIntroduction: "",
+            intermediaryLogoUrl: "",
+            intermediaryName: "",
+            isPlat: 1,
+            updateTime: "",
+            user:{
+                id: 1,
+                name: "",
+                mobile: ""
+            }
+        }
     },
     methods:{
         addDevice:function(){
@@ -99,7 +154,89 @@ new Vue({
                     alert(1);
                 }
             });
-        }
+        },
+        searchZtree:function(){
+            var searchVal = this.searchVal;
+            if(searchVal.length != 0){
+                $.axspost(contextPath + "area/?shortName="+searchVal,"get","",function (data) {
+                    var getData = data.data;
+                    $(".search-list").html("");
+                    if(getData.length != 0){
+                        getData.forEach(function(item,i){
+                            var li = "<li data-id="+getData[i].id+">"+getData[i].name+"</li>";
+                            $(".search-list").append(li).show();
+                            $("#treeDemo").hide();
+                        })
+                    }
+                },function (data) {
+
+                });
+            }else{
+                $("#treeDemo").show();
+                $(".search-list").hide();
+            }
+
+        },
+        zTreeInit:function(){
+            var _this = this;
+            return {
+                useTree : function(ele,zNodes,inputEle){
+                    var setting = {
+                        async: {
+                            enable: true,
+                            url: getUrl,
+                            type: "get",
+                            dataFilter: ajaxDataFilter
+                        },
+                        data: {
+                            simpleData: {
+                                enable: true
+                            }
+                        },
+                        callback: {
+                            onClick:zTreeOnclick,
+                            beforeExpand: beforeExpand,
+                            onAsyncSuccess:onAsyncSuccess
+                        },
+                        view: {
+                            fontCss: getFontCss,
+                            showLine: false
+                            // showIcon:false
+                        }
+                    };
+                    function getFontCss(treeId, treeNode) {
+                        return (!!treeNode.highlight) ? {color:"#A60000", "font-weight":"bold"} : {color:"#333", "font-weight":"normal"};
+                    }
+                    //异步加载返回的数据
+                    function ajaxDataFilter(treeId, parentNode, responseData) {
+                        return responseData.data;
+                    }
+                    function zTreeOnclick(event, treeId, treeNode) {
+                        _this.editList.area.name = treeNode.name;
+                        _this.editList.area.id = treeNode.id;
+                        // _this.treeIsShow = false;
+                        $(".tree-div-contain").hide();
+                    }
+                    function getUrl(treeId, treeNode){
+                        return contextPath +"area/condition?level="+parseInt(treeNode.level+2)+"&parent="+parseInt(treeNode.id);
+                    }
+                    function beforeExpand(treeId, treeNode) {
+                        if (!treeNode.isAjaxing) {
+                            return true;
+                        } else {
+                            alert("zTree 正在下载数据中，请稍后展开节点。。。");
+                            return false;
+                        }
+                    }
+                    function onAsyncSuccess(event, treeId, treeNode, msg) {
+                        var treeObj = $.fn.zTree.getZTreeObj("treeDemo");
+                        treeObj.updateNode(treeNode);
+                    }
+                    $.fn.zTree.init(ele, setting, zNodes);
+                }
+            }
+
+        },
     },
     mounted:function(){
         var _this = this;
@@ -111,8 +248,10 @@ new Vue({
         $(document).on("click","#detail",function(){
             window.location.href = "detail?menuId=30";
         });
-
-        //
+        $.axspost(contextPath + "area/condition?level=1","get","",function(data){
+            _this.facList = data.data;
+            _this.zTreeInit().useTree($("#treeDemo"),_this.facList,"#address");//初始化生成树
+        },function (data) {});
         _this.tableInit(false); //创建表格
 
         //
