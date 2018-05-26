83145772b1dfc0252db5cd42bf7aad19691eb120
zhongke1214 <38214294+zhongke1214@users.noreply.github.com>
2018-05-26 12:13:24 +0800

    重复异常抓取

--- src/main/java/com/jianpanmao/common/advisor/GlobalExceptionHandler.java
+++ src/main/java/com/jianpanmao/common/advisor/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.jianpanmao.common.advisor;
 
 import com.jianpanmao.common.entity.ResultEntity;
 import com.jianpanmao.exception.AccessTokenException;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -36,4 +37,12 @@ public class GlobalExceptionHandler {
         response.setStatus(HttpStatus.OK.value());
         return new ResultEntity(10001,"content error", "appId或appSecret错误");
     }
+
+    @ExceptionHandler(MySQLIntegrityConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.OK)
+    public ResultEntity existException(HttpServletResponse response,MySQLIntegrityConstraintViolationException e){
+        response.setStatus(HttpStatus.OK.value());
+        return new ResultEntity(10000,e.getMessage(),"不被允许重复内容");
+    }
+
 }
