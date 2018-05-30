9f48820c3c6f9afd48f1415ea23e4bcb32cdd0df
zhongke1214 <38214294+zhongke1214@users.noreply.github.com>
2018-05-30 23:09:04 +0800

    数据统计 中间商设备数量统计

---
+++ src/main/java/com/jianpanmao/dataStatistics/controller/DataStatisticsRestController.java
@@ -0,0 +1,26 @@
+package com.jianpanmao.dataStatistics.controller;
+
+import com.jianpanmao.dataStatistics.dao.DataStatisticsMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by mayn on 2018/5/30.
+ * 数据统计
+ */
+@RestController
+@RequestMapping("/api/dataStatistics")
+public class DataStatisticsRestController {
+    @Autowired
+    DataStatisticsMapper dao;
+
+    @GetMapping("/intermediaryDeviceCount")
+        public List<Map> intermediaryDeviceCount(){
+        return dao.IntermediaryDeviceCount();
+    }
+}
---
+++ src/main/java/com/jianpanmao/dataStatistics/dao/DataStatisticsMapper.java
@@ -0,0 +1,14 @@
+package com.jianpanmao.dataStatistics.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by mayn on 2018/5/30.
+ */
+@Mapper
+public interface DataStatisticsMapper {
+    List<Map> IntermediaryDeviceCount();
+}
