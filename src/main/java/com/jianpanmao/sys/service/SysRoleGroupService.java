2feeaf22abc6b0a3676689a426f0b0f7bcd6a70f
lijieaa <952054767@qq.com>
2018-06-02 12:30:00 +0800

    角色组添加，修改

---
+++ src/main/java/com/jianpanmao/sys/service/SysRoleGroupService.java
@@ -0,0 +1,12 @@
+package com.jianpanmao.sys.service;
+
+import com.jianpanmao.common.service.BaseService;
+import com.jianpanmao.sys.entity.*;
+import com.jianpanmao.sys.dto.*;
+
+
+
+
+public interface SysRoleGroupService extends BaseService<SysRoleGroup, SysRoleGroupExample,SysRoleGroupDto,Integer>{
+
+}
