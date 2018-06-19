106dcc90daa7e1a62dbac169d914135d51e720f9
ltchao <38102946+ltchao@users.noreply.github.com>
2018-06-19 23:01:34 +0800

    菜单页面

--- src/main/resources/static/dist/js/gzh/menu.js
+++ src/main/resources/static/dist/js/gzh/menu.js
@@ -8,36 +8,42 @@
 
 
 new Vue({
-  el:"#menu",
+  el:"#app",
   data:{
     selectedMenuIndex:'',//当前选中菜单索引
     selectedSubMenuIndex:'',//当前选中子菜单索引
-    // postMsg:true,
-    // view:false,
-    // currentSelect:'',
     menu:{
-      button:[
-
-      ]
+      button:[]
     }
   },
   methods:{
     addMenu:function(level){
       if(level == 1 && this.menu.button.length<3){
         this.menu.button.push({
-          "name":"菜单名称",
-          "isSelect":"postMsg",
-           "type":"msg",
-          "childMenu":[]
-        })
+            "name":"菜单名称",
+            "isSelect":"postMsg",
+            "type":"msg",
+            "url":"",
+            "msgValue":"",
+            "fontValue":"",
+            "photoValue":"",
+            "voiceValue":"",
+            "childMenu":[]
+        });
         this.selectedMenuIndex = this.menu.button.length-1;
         this.selectedSubMenuIndex=''
       }
       if(level == 2 && this.menu.button[this.selectedMenuIndex].childMenu.length<5){
         this.menu.button[this.selectedMenuIndex].childMenu.push({
-          name:"子菜单名称",
-          "isSelect":true
-        })
+            "name":"子菜单名称",
+            "isSelect":"postMsg", //选中哪一个菜单
+            "type":"msg",
+            "url":"",
+            "msgValue":"",
+            "fontValue":"",
+            "photoValue":"",
+            "voiceValue":""
+        });
         this.selectedSubMenuIndex= this.menu.button[this.selectedMenuIndex].childMenu.length-1;
 
         // this.selectedMenuIndex = '';
@@ -46,16 +52,16 @@ new Vue({
       }
     },
     selectedMenu:function(i,event){ //选择主菜单
-      this.selectedSubMenuIndex=''
-      this.selectedMenuIndex=i
-      var selectedMenu = this.menu.button[this.selectedMenuIndex];
+      this.selectedSubMenuIndex='';
+      this.selectedMenuIndex=i;
+      // var selectedMenu = this.menu.button[this.selectedMenuIndex];
       if(this.selectedMenuLevel() == 1){
-        // this.selectTab();
+        this.selectTab();
       }
     },
     selectedSubMenu:function(i,event){//选择子菜单
       this.selectedSubMenuIndex = i;
-
+        this.selectTab();
     },
     //选中菜单级别
     selectedMenuLevel: function () {
@@ -73,44 +79,102 @@ new Vue({
     submitData:function(){
       console.log(this.menu);
     },
-    selectTab:function(){
-      switch (this.menu.button[this.selectedMenuIndex].isSelect){
-        case 'postMsg':
-          return 1;
-          break;
-        case 'view':
-          return 2;
-          break;
+    selectTab:function(){ //选中发送消息还是发送网页
+      if(this.selectedMenuLevel() == 1) {
+          switch (this.menu.button[this.selectedMenuIndex].isSelect) {
+              case 'postMsg':
+                  this.menu.button[this.selectedMenuIndex].url = "";
+                  return 1;
+                  break;
+              case 'view':
+                  this.menu.button[this.selectedMenuIndex].msgValue = "";
+                  this.menu.button[this.selectedMenuIndex].fontValue = "";
+                  this.menu.button[this.selectedMenuIndex].photoValue = "";
+                  this.menu.button[this.selectedMenuIndex].voiceValue = "";
+                  return 2;
+                  break;
+          }
+      }else{
+          switch (this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].isSelect) {
+              case 'postMsg':
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].url = "";
+                  return 1;
+                  break;
+              case 'view':
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].msgValue = "";
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].fontValue = "";
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].photoValue = "";
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].voiceValue = "";
+                  return 2;
+                  break;
+          }
       }
+
     },
-    subMenu:function(msg){
-      switch (msg){
-        case 'msg' :
-          this.menu.button[this.selectedMenuIndex].type = 'msg';
-          break;
-        case 'font' :
-          this.menu.button[this.selectedMenuIndex].type = 'font';
-          break;
-        case 'photo' :
-          this.menu.button[this.selectedMenuIndex].type = 'photo';
-          break;
-        case 'voice' :
-          this.menu.button[this.selectedMenuIndex].type = 'voice';
-          break;
+    subMenu:function(level,msg){
+      if(level == 1){
+          switch (msg){
+              case 'msg' :
+                  this.menu.button[this.selectedMenuIndex].type = 'msg';
+                  break;
+              case 'font' :
+                  this.menu.button[this.selectedMenuIndex].type = 'font';
+                  break;
+              case 'photo' :
+                  this.menu.button[this.selectedMenuIndex].type = 'photo';
+                  break;
+              case 'voice' :
+                  this.menu.button[this.selectedMenuIndex].type = 'voice';
+                  break;
+          }
+      }else{
+          switch (msg){
+              case 'msg' :
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].type = 'msg';
+                  break;
+              case 'font' :
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].type = 'font';
+                  break;
+              case 'photo' :
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].type = 'photo';
+                  break;
+              case 'voice' :
+                  this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].type = 'voice';
+                  break;
+          }
       }
+
     },
+      //主菜单选中哪一个type
     currentSelect:function(){
-      if(this.menu.button[this.selectedMenuIndex].type == 'msg'){
+      var menuIndex = this.menu.button[this.selectedMenuIndex].type;
+      if(menuIndex == 'msg'){
         return 1
-      }else if(this.menu.button[this.selectedMenuIndex].type == 'font'){
+      }else if(menuIndex == 'font'){
         return 2
       }
-      else if(this.menu.button[this.selectedMenuIndex].type == 'photo'){
+      else if(menuIndex == 'photo'){
         return 3
       }
       else{
         return 4
       }
+    },
+
+  //右边子菜单选中哪一个type
+    currentSubSelect:function(){
+        var submenuIndex = this.menu.button[this.selectedMenuIndex].childMenu[this.selectedSubMenuIndex].type;
+        if(submenuIndex == 'msg'){
+            return 1
+        }else if(submenuIndex == 'font'){
+            return 2
+        }
+        else if(submenuIndex == 'photo'){
+            return 3
+        }
+        else{
+            return 4
+        }
     },
     fromSelect:function() {
       layer.open({
@@ -120,10 +184,33 @@ new Vue({
         id: 'materialList', //防止重复弹出
         content: '<div style="padding: 20px 100px;">66666</div>'
       });
+    },
+    //删除主菜单菜单
+    delMenu:function () {
+          if(this.selectedMenuIndex===0){
+              this.menu.button.splice(this.selectedMenuIndex, 1);
+              this.selectedMenuIndex = 0;
+          }
+          if(this.menu.button.length==0){
+              this.selectedMenuIndex = ''
+          }
+    },
+    delSubMenu:function(){
+        if(this.selectedSubMenuIndex===0){
+            this.menu.button[this.selectedMenuIndex].childMenu.splice(this.selectedSubMenuIndex, 1);
+            this.selectedSubMenuIndex = 0;
+        }else{
+            this.menu.button[this.selectedMenuIndex].childMenu.splice(this.selectedSubMenuIndex, 1);
+            this.selectedSubMenuIndex -= 1;
+        }
+        if(this.menu.button[this.selectedMenuIndex].childMenu.length==0){
+            this.selectedSubMenuIndex = ''
+        }
     }
   },
   mounted:function () {
     var _this =  this;
     // _this.selectTab(1);
   }
-})
+
+});
