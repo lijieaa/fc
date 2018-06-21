0d6d1f5de6f7a53420025e6a0449e7d7aa5fa4b2
zhongke1214 <38214294+zhongke1214@users.noreply.github.com>
2018-06-21 22:51:44 +0800

    修改统计页

---
+++ src/main/java/com/jianpanmao/dataStatistics/dto/DataStatisticsDto.java
@@ -0,0 +1,25 @@
+package com.jianpanmao.dataStatistics.dto;
+
+/**
+ * Created by mayn on 2018/6/21.
+ */
+public class DataStatisticsDto {
+    private Integer year;
+    private Integer count;
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+}
