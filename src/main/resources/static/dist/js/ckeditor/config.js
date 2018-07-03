384e801bcbee4c37e1d01d741d186c929b25c2a6
liuhong <1150487420@qq.com>
2018-07-03 22:27:38 +0800

    编辑中间商详情样式修改

--- src/main/resources/static/dist/js/ckeditor/config.js
+++ src/main/resources/static/dist/js/ckeditor/config.js
@@ -16,7 +16,7 @@ CKEDITOR.editorConfig = function( config ) {
         { name: 'paragraph', items : [ 'NumberedList','BulletedList','-','Outdent','Indent','-','Blockquote','CreateDiv',
             '-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','-','BidiLtr','BidiRtl' ] },
         { name: 'links', items : [ 'Link','Unlink','Anchor' ] },
-        { name: 'insert', items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
+        { name: 'insert', items : [ 'Image','Table','HorizontalRule','Smiley','SpecialChar','PageBreak','Iframe' ] },
         '/',
         { name: 'styles', items : [ 'Styles','Format','Font','FontSize' ] },
         { name: 'colors', items : [ 'TextColor','BGColor' ] },
