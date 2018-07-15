44d49d3d1b8f21dff7668e92427c6a2a97e8356a
lijieaa <952054767@qq.com>
2018-07-15 09:37:57 +0800

    角色客户通信录是否可见

--- src/main/java/com/jianpanmao/sys/service/SysRoleService.java
+++ src/main/java/com/jianpanmao/sys/service/SysRoleService.java
@@ -4,11 +4,13 @@ import com.jianpanmao.common.service.BaseService;
 import com.jianpanmao.sys.entity.*;
 import com.jianpanmao.sys.dto.*;
 
-
+import java.util.List;
 
 
 public interface SysRoleService extends BaseService<SysRole, SysRoleExample,SysRoleDto,Integer>{
     public int unbindUser(Integer userId);
 
     int deleteByGroupId(Integer gid);
+
+    List<SysRole> findByIsCon(Integer isCon);
 }
