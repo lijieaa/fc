4c227c708e1a76e8bdb1fe5e01f3684658acf0cf
zdcsoul <451914442@qq.com>
2018-07-16 22:01:13 +0800

    设备列表接口

---
+++ src/main/java/com/jianpanmao/wechat/wx/WxDeviceRestController.java
@@ -0,0 +1,47 @@
+package com.jianpanmao.wechat.wx;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jianpanmao.common.entity.DataTablesResponseEntity;
+import com.jianpanmao.device.dto.DeviceDto;
+import com.jianpanmao.device.entity.Device;
+import com.jianpanmao.device.service.DeviceService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author zdc
+ * @since 2018-07-16 20:53
+ */
+@RequestMapping("wd")
+@RestController
+public class WxDeviceRestController {
+
+    @Autowired private DeviceService deviceService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "devices")
+    public Object page(@RequestParam(value = "pageNum", defaultValue = "1", required = true) Integer pageNum,
+                       @RequestParam(value = "pageSize", defaultValue = "10", required = true) Integer pageSize,
+                       @RequestParam(value = "draw", required = false) Integer draw,
+                       DeviceDto dto) {
+
+        PageHelper.startPage(pageNum, pageSize);
+
+        List<Device> list = deviceService.getByDto(dto);
+        PageInfo pageInfo = new PageInfo(list);
+
+//draw 不等于空是datatables分页
+        if (draw != null) {
+            DataTablesResponseEntity<Device> responseEntity = new DataTablesResponseEntity(draw, pageInfo.getTotal(), pageInfo.getTotal(), pageInfo.getList());
+            return responseEntity;
+        } else {
+            return pageInfo;
+        }
+
+    }
+}
