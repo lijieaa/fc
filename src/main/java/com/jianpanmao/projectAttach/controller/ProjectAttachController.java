06ec34c99f39a9fbcb74e0d4eef2821309ed61b0
zhongke1214 <38214294+zhongke1214@users.noreply.github.com>
2018-07-26 20:55:32 +0800

    项目资源

--- src/main/java/com/jianpanmao/projectAttach/controller/ProjectAttachController.java
+++ src/main/java/com/jianpanmao/projectAttach/controller/ProjectAttachController.java
@@ -1,8 +1,15 @@
 package com.jianpanmao.projectAttach.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jianpanmao.projectAttach.dao.ProjectAttachDao;
+import com.jianpanmao.projectAttach.entity.AttachType;
+import com.jianpanmao.projectAttach.entity.PType;
+import com.jianpanmao.projectAttach.entity.ProjectPtype;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by mayn on 2018/7/25.
@@ -10,11 +17,68 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/pa")
 public class ProjectAttachController {
+
+    @Autowired
+    private ProjectAttachDao dao;
+
+    /****
+     * 资源树
+     */
+    @GetMapping("pType")
+    public List<PType> pTypeTree(){
+        List<PType> pTypes = dao.allPtype();
+        List<PType> tops = new ArrayList<>();
+        for (PType p:pTypes){
+            if (p.getpTypeParentId().intValue()==0){
+                tops.add(p);
+            }
+        }
+        pTypes.removeAll(tops);
+return null;
+    }
+
     /**
      * 新增项目文件分类
      */
     @PostMapping("pType")
-    public void pType(){
-
+    @Transactional
+    public void addPType(String name,Integer parentId,Integer projectId){
+        PType pType = new PType();
+        pType.setpTypePName(name);
+        pType.setpTypeParentId(parentId);
+        if (pType.getpTypeParentId().intValue()==0){
+            pType.setpTypePPath(",0,");
+        }else {
+            PType parent = dao.selectPType(pType.getpTypeParentId());
+            String path = parent.getpTypePPath();
+            path += path+parent.getpTypeId()+",";
+            pType.setpTypePPath(path);
+        }
+        dao.insertPType(pType);
+        //关联项目
+        ProjectPtype projectPtype = new ProjectPtype();
+        projectPtype.setProjectPTypePId(pType.getpTypeId());
+        projectPtype.setProjectPTypeTId(projectId);
+        dao.insertProjectPType(projectPtype);
     }
+
+    /**
+     * 修改项目文件分类名
+     */
+    @PutMapping("pType")
+    public void updatePType(PType pType) {
+        dao.updatePType(pType);
+    }
+
+    /**
+     * 上传文件
+     */
+    @PostMapping("attachType")
+    public void addAttachType(Integer pTypeId,Integer attachId){
+        AttachType attachType = new AttachType();
+        attachType.setAttachTypeTId(pTypeId);
+        attachType.setAttachTypeAId(attachId);
+        dao.insertAttachType(attachType);
+    }
+
 }
---
+++ src/main/java/com/jianpanmao/projectAttach/dao/ProjectAttachDao.java
@@ -0,0 +1,23 @@
+package com.jianpanmao.projectAttach.dao;
+
+import com.jianpanmao.projectAttach.entity.AttachType;
+import com.jianpanmao.projectAttach.entity.PType;
+import com.jianpanmao.projectAttach.entity.ProjectPtype;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * Created by mayn on 2018/7/26.
+ */
+@Mapper
+public interface ProjectAttachDao {
+    Integer insertPType(PType pType);
+    PType selectPType(Integer id);
+    void updatePType(PType pType);
+    List<PType> allPtype();
+
+    Integer insertProjectPType(ProjectPtype projectPtype);
+
+    void insertAttachType(AttachType attachType);
+}
